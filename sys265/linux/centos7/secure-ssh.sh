7a39d3163a624f798cf207c3e6e88b99ff42cdb3
wAlber47 <walber47@gmail.com>
2020-10-09 09:17:48 -0400

    added linux folder

---
+++ sys265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author walber
+#creates a new SSH user using $1 parameter
+#adds a public key from the local repo or curled from the rmote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
