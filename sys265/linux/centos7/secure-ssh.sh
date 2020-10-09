59080e7d3f28452fa911b9cc3398b02eaf024b85
wAlber47 <walber47@gmail.com>
2020-10-09 09:21:48 -0400

    changes

---
+++ sys265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,5 @@
+#author walber
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
